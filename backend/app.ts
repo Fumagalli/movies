cc2d0835b13846927699f140efa9dfa30119eb46
Ângelo Fumagalli <angelo.fumagalli@atlastechnol.com>
2025-05-25 18:53:46 -0300

    chore: updates to make build work without issue

--- backend/app.ts
+++ backend/app.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
-import * as express from "express";
-import * as cors from "cors";
+import { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+import path from "path";
+
 import { AppDataSource } from "./src/data-source";
 import userRoutes from "./src/routes/user.routes";
 import movieRoutes from "./src/routes/movie.routes";
 import filterRoutes from "./src/routes/filter.routes";
-import * as path from "path";
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,14 +19,16 @@ app.use("/movies", movieRoutes);
 app.use("/filters", filterRoutes);
 app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-AppDataSource.initialize().then(() => {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}).catch((err) => {
-  console.error("Database connection failed:", err);
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
